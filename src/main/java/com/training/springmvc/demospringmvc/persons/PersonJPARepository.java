15d270920b151a9f9086fbc45d0a2ef3ebc3f526
Delia Popescu <dfotescu@gmail.com>
2018-03-27 15:08:50 +0300

    demo spring mvc

---
+++ src/main/java/com/training/springmvc/demospringmvc/persons/PersonJPARepository.java
@@ -0,0 +1,6 @@
+package com.training.springmvc.demospringmvc.persons;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonJPARepository extends JpaRepository<Person, Integer>{
+
+}
